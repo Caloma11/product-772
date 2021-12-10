daa077093431ce270d5a17a359275591df07bfd7
gianluca perrone <perronegian@hotmail.com>
2021-12-10 09:19:10 +0800

    a

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+product_names = ["Red Truck", "Blue Bottle", "White Ball", "Green Hat", "Yellow Shoes"]
+
+puts 'Creating 5 products...'
+product_names.each do |name|
+
+  product = Product.create!(
+    name: name,
+    tagline: name.chars.shuffle.join + name.chars.shuffle.join
+  )
+
+end
+
+
